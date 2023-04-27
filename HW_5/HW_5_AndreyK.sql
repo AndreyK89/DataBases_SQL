e3f0a1a8e806af9baf0090c8ec74fe1ff16ebe30
Andrey_K <kaa89job@yandex.ru>
2023-04-28 01:13:07 +0500

    Edit HW_5_AndreyK.sql

--- HW_5/HW_5_AndreyK.sql
+++ HW_5/HW_5_AndreyK.sql
@@ -1,3 +1,4 @@
+-- Задача 1.
 DROP DATABASE IF EXISTS hw_5;
 CREATE DATABASE hw_5;
 USE hw_5;
@@ -23,7 +24,7 @@ VALUES
 SELECT *
 FROM cars;
 
--- Задача 1. Создайте представление, в которое попадут автомобили стоимостью до 25 000 долларов.
+-- 1. Создайте представление, в которое попадут автомобили стоимостью до 25 000 долларов.
 
 CREATE VIEW view_cars AS
 SELECT *
@@ -33,7 +34,7 @@ WHERE cost < 25000;
 SELECT *
 FROM view_cars;
 
--- Задача 2. Измените в существующем представлении порог для стоимости: пусть цена будет до 30 000 долларов (используя оператор ALTER VIEW).
+-- 2. Измените в существующем представлении порог для стоимости: пусть цена будет до 30 000 долларов (используя оператор ALTER VIEW).
 
 ALTER VIEW view_cars AS
 SELECT *
@@ -43,7 +44,7 @@ WHERE cost < 30000;
 SELECT *
 FROM view_cars;
 
--- Задача 3. Создайте представление, в котором будут только автомобили марки “Шкода” и “Ауди”.
+-- 3. Создайте представление, в котором будут только автомобили марки “Шкода” и “Ауди”.
 
 CREATE VIEW view_cars_2 AS
 SELECT *
@@ -51,4 +52,39 @@ FROM cars
 WHERE name IN ("Skoda", "Audi");
 
 SELECT *
-FROM view_cars_2
+FROM view_cars_2;
+
+-- Задача 2.
+
+/*
+Добавьте новый столбец под названием «время до следующей станции». 
+Чтобы получить это значение, мы вычитаем время станций для пар смежных станций. 
+Мы можем вычислить это значение без использования оконной функции SQL, 
+но это может быть очень сложно. Проще это сделать с помощью оконной функции LEAD . 
+Эта функция сравнивает значения из одной строки со следующей строкой, чтобы получить 
+результат. В этом случае функция сравнивает значения в столбце «время» для станции со 
+станцией сразу после нее.
+*/
+
+CREATE TABLE stations
+(
+train_id INT NOT NULL,
+station varchar(20) NOT NULL,
+station_time TIME NOT NULL
+);
+
+INSERT INTO stations(train_id, station, station_time)
+VALUES (110, "SanFrancisco", "10:00:00"),
+(110, "Redwood Sity", "10:54:00"),
+(110, "Palo Alto", "11:02:00"),
+(110, "San Jose", "12:35:00"),
+(120, "SanFrancisco", "11:00:00"),
+(120, "Palo Alto", "12:49:00"),
+(120, "San Jose", "13:30:00");
+
+SELECT * FROM stations;
+
+SELECT 
+	stations.*,
+	TIMEDIFF (LEAD(station_time) OVER (PARTITION BY train_id), station_time) AS time_to_next_station
+FROM stations
